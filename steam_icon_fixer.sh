3352db4b9c4ea89564d0a5f3dc8e8e7cef369646
TheToasterVTO <160161760+TheToasterVTO@users.noreply.github.com>
2025-07-18 08:49:29 -0600

    Update steam_icon_fixer.sh

--- steam_icon_fixer.sh
+++ steam_icon_fixer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Adds "StartupWMClass=steam_app_<id>" to all .desktop files located in the DIR specified (default is the directory the script currently is in) 
-# to resolve missing missing game icons on Steam games in GNOME applications overview and task switcher.
+# to resolve missing game icons on Steam games in GNOME applications overview and task switcher.
 # This is a modified verison of https://gitlab.com/-/snippets/3763640 to allow automatic execution.
 
 
